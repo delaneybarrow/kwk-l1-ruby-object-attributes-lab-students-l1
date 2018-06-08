faa80f5f34090f27ca0d76f0db313e0875d35bb6
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-08 17:34:31 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # person.rb
+class Person  
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(person_job)
+    @job = person_job
+  end 
+  
+  def job
+    @job
+  end
+  
+end
+
+delaney = Person.new 
+delaney.name = "delaney"
+delaney.job = "student"
